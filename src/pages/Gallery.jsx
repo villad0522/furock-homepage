542402c20cd2e3a616b28f842c5901d3c64a65fd
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2022-03-11 11:59:33 +0900

    a

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -13,8 +13,29 @@ export default function Gallery({ fileName }) {
     const dispatch = useDispatch();
     const files = useSelector(state => state?.json?.files);
     const theme = useTheme();
-    const isLandscape = useMediaQuery(theme.breakpoints.up('sm'));
-    const category = isLandscape ? "ページ（パソコン）" : "ページ（スマホ）";
+    const mdUp = useMediaQuery(theme.breakpoints.up('md'));
+    const smUp = useMediaQuery(theme.breakpoints.up('sm'));
+    const category = React.useMemo(() => {
+        // クエリパラメータを取得
+        var params = new URL(window.location.href).searchParams;
+        const media = params.get('media');
+        if (media === 'pc') {
+            return "ページ（パソコン）";
+        }
+        else if (media === 'tablet') {
+            return "ページ（タブレット）";
+        }
+        else if (media === 'mobile') {
+            return "ページ（スマホ）";
+        }
+        else if (mdUp) {
+            return "ページ（パソコン）";
+        }
+        else if (smUp) {
+            return "ページ（タブレット）";
+        }
+        return "ページ（スマホ）";
+    }, [mdUp, smUp]);
     const categories = files[fileName] ? { ...files[fileName] } : {};
     if (!categories["一般設定"]) {
         return null;
